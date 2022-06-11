9d583e87a8706d36d1d0f156c30da387d1f4efea
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-06-11 16:00:25 +0500

    post function added and some installation performed like bod y parser

--- calc.js
+++ calc.js
@@ -1,14 +1,21 @@
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
+// const { urlencoded } = require("body-parser");
 
-const app = express()
+const app = express();
+app.use(bodyParser, urlencoded({ extended: true }));
 
-app.listen(3000,function(){
-    console.log("3000 port in successfully Active")
+app.listen(3000, function () {
+  console.log("3000 port in successfully Active");
 });
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname+'/home.html')
-    // console.log(__dirname)
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/home.html");
+  // console.log(__dirname)
+});
 
-})
-
+app.post("/", function (req, res) {
+  console.log(req.body);
+  res.send("thanks for posting ");
+});
+ 
