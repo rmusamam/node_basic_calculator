adc9904ed6b3208d75da1636c0201a6caec7575c
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-06-11 22:10:05 +0500

    form data showed

--- calc.js
+++ calc.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const { urlencoded } = require("body-parser");
-
 const app = express();
-app.use(bodyParser, urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/home.html");
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body.ln);
+  res.send("thanks for posting ");
+});
 
 app.listen(3000, function () {
   console.log("3000 port in successfully Active");
 });
-
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/home.html");
-  // console.log(__dirname)
-});
-
-app.post("/", function (req, res) {
-  console.log(req.body);
-  res.send("thanks for posting ");
-});
- 
